00553cef3d93ad7f4898444c612237aa488f7ccd
iantheone234 <hsalamanc6185@gmail.com>
2025-02-26 00:35:26 -0700

    Create HeldFormInteract.java

---
+++ src/main/java/git/dragomordor/megamons/item/custom/HeldFormInteract.java
@@ -0,0 +1,10 @@
+package git.dragomordor.megamons.item.custom;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+
+public class HeldFormInteract extends Item {
+    public HeldFormInteract() {
+        super(new FabricItemSettings().maxCount(1));
+    }
+}
